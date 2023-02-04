4dfa14b42ff6b8d97b3325eb16e91d3a7fd701ac
Etienne Beaulieu <ety_95@protonmail.com>
2023-02-04 13:21:58 -0500

    Remove comment and fix warning

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -464,7 +464,6 @@ mason_lspconfig.setup_handlers {
         home..'/.cache/jdtls/config',
         '-data',
         home..'/.cache/jdtls/workspace'
-        -- home..'/repos/workspace/coreapi'
       },
       on_attach = on_attach,
     }
@@ -487,7 +486,7 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-Space>'] = cmp.mapping.complete({}),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
