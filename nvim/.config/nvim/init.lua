ff6e37d7f8b134f0ea43f4a373da12d559a9781a
Etienne Beaulieu <ety_95@protonmail.com>
2023-03-09 10:59:51 -0500

    Add shortcuts for harpoon

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -338,6 +338,16 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
+local harpoon_mark = require("harpoon.mark")
+local harpoon_ui = require("harpoon.ui")
+vim.keymap.set('n', '<leader>m', harpoon_mark.add_file)
+vim.keymap.set('n', '<leader>n', harpoon_ui.toggle_quick_menu)
+
+vim.keymap.set('n', '<leader>h', function () harpoon_ui.nav_file(1) end)
+vim.keymap.set('n', '<leader>j', function () harpoon_ui.nav_file(2) end)
+vim.keymap.set('n', '<leader>k', function () harpoon_ui.nav_file(3) end)
+vim.keymap.set('n', '<leader>l', function () harpoon_ui.nav_file(4) end)
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
@@ -365,7 +375,6 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
 
   end
-
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
