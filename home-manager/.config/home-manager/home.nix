550b98c49d8d74bb8b09cda8182d702b6c7b073e
Etienne Beaulieu <ety_95@protonmail.com>
2023-05-29 16:52:25 -0400

    Fix home-manager config

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -7,8 +7,8 @@ in
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "$username";
-  home.homeDirectory = "/home/$username";
+  home.username = "${username}";
+  home.homeDirectory = "/home/${username}";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
