08807b393f5b32419a73b1e67b07da7935c61a37
Etienne Beaulieu <ety_95@protonmail.com>
2023-10-14 22:35:40 -0400

    Update blankline indent config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -63,7 +63,14 @@ require('packer').startup(function(use)
   use 'lewis6991/gitsigns.nvim'
 
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
+  use {
+    'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+    main = "ibl",
+    opts = {
+      char = '┊',
+      show_trailing_blankline_indent = false
+    }
+  }
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
 
@@ -193,13 +200,6 @@ require('Comment').setup({
 
 
 })
-
--- Enable `lukas-reineke/indent-blankline.nvim`
--- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
-}
 
 
 -- Gitsigns
