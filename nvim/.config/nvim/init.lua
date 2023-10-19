efa023f8df0af892b1e027595976e09d934b4110
Etienne Beaulieu <ety_95@protonmail.com>
2023-10-19 13:35:43 -0400

    Remove comment

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -261,7 +261,6 @@ vim.keymap.set('n', '<C-p>', function ()
   })
 end, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
--- vim.keymap.set('n', '<leader>sw', require('telescope.builtig').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>f', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>a', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
