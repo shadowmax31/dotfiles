0e3c5af2eed6bc1723952dc5251327a8deb23567
Etienne Beaulieu <ety_95@protonmail.com>
2024-02-09 09:50:38 -0500

    Change shortcut for buffer search

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -235,7 +235,7 @@ pcall(require('telescope').load_extension, 'fzf')
 vim.keymap.set('n', '<leader>cd', ':e '..config..'/init.lua<CR>')
 
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><leader>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>d', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
