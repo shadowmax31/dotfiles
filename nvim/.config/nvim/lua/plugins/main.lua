0d4ef0ff27097fe56f2da93b2b6e24eb06aa9fca
Etienne Beaulieu <ety_95@protonmail.com>
2024-08-02 23:41:25 -0400

    Improve lualine

--- nvim/.config/nvim/lua/plugins/main.lua
+++ nvim/.config/nvim/lua/plugins/main.lua
@@ -14,12 +14,22 @@ return {
 
   {
     'nvim-lualine/lualine.nvim',
+    dependencies = 'folke/noice.nvim',
     opts = {
       options = {
         icons_enabled = true,
         theme = 'dracula',
         component_separators = '|',
         section_separators = '',
+      },
+      sections = {
+        lualine_a = {},
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+          }
+        },
       },
     }
   },
