f90dbbb1bf550144fb6b6eb324d8301a69e142d8
Etienne Beaulieu <ety_95@protonmail.com>
2025-02-19 10:16:01 -0500

    wm - Add calendar notification

--- home-manager/.config/home-manager/system-information.nix
+++ home-manager/.config/home-manager/system-information.nix
@@ -8,12 +8,28 @@ pkgs.writeTextFile {
   text = ''
   #!/bin/sh
 
-  if [ "`timew`" = "There is no active time tracking." ]; then
-    time_warrior="inactive"
+  title=""
+  body=""
+  if [ "$1" = "cal" ]; then
+    today=$(date '+%-d')
+    title=$(cal | head -n1 | awk '$1=$1')
+	  body=$(cal | tail -n7 | sed -z "s|$today|<u><b>$today</b></u>|1")
+
   else
-    time_warrior=`timew sum | tail -n2 | head -n1 | xargs | rev | cut -c4- | rev`
+    if [ "`timew`" = "There is no active time tracking." ]; then
+      time_warrior="inactive"
+    else
+      time_warrior=`timew sum | tail -n2 | head -n1 | xargs | rev | cut -c4- | rev`
+    fi
+
+    title="System Information"
+    body=" $(date '+%Y-%m-%d %R:%S')\n $time_warrior"
   fi
+
+
+  notify-send -i /usr/share/void-artwork/void-transparent.png \
+    "$title" \
+    "$body"
   
-  notify-send -i /usr/share/void-artwork/void-transparent.png "System Information" " $(date '+%Y-%m-%d %R:%S')\n $time_warrior"
   '';
 }
