e7f31639c7d1c771a968a5818bc2c546e1682c6a
Etienne Beaulieu <ety_95@protonmail.com>
2025-04-22 09:56:04 -0400

    nvim - Remove ai.lua
    I don't use it

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -24,6 +24,10 @@ in
     })
   ];
 
+  # AI - Ollama
+  # docker run -d -p 3003:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+  # docker run -e  HSA_OVERRIDE_GFX_VERSION=10.3.0 -d --device /dev/kfd --device /dev/dri -v ollama:/root/.ollama -p 11434:11434 --name ollama --restart always ollama/ollama:rocm
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "${username}";
