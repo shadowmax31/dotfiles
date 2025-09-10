3908df0cdeb70981091b80cf5601bce5b6202321
Etienne Beaulieu <ety_95@protonmail.com>
2025-09-10 11:09:32 -0400

    nvim - fix warning when using gradle

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -121,6 +121,29 @@ return {
             '-data',
             home .. '/.cache/jdtls/workspace'
           },
+
+          init_options = {
+            settings = {
+              java = {
+                imports = {
+                  gradle = {
+                    enabled = true,
+                    wrapper = {
+                      enabled = true,
+                      checksums = {
+                        {
+                          sha256 = '7d3a4ac4de1c32b59bc6a4eb8ecb8e612ccd0cf1ae1e99f66902da64df296172',
+                          allowed = true
+                        }
+                      },
+                    }
+                  }
+                }
+              },
+            },
+          }
+
+
         },
         pest_ls = {},
         pylsp = {
