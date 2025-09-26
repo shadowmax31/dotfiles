3979d7bb990388b9f535f0c977ebb70896c683b0
Etienne Beaulieu <ety_95@protonmail.com>
2025-09-26 14:11:55 -0400

    nvim - use treesitter for syntax highlight

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,13 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'antoinephilibert/tree-sitter-w'
+    },
     build = ':TSUpdate',
     config = function()
-      require'nvim-treesitter.configs'.setup {
-        ensure_installed = { 'c', 'cpp', 'lua', 'rust', 'typescript', 'vimdoc', 'vim', 'java', 'scss', 'bash', 'html', 'markdown', 'yaml' },
+      require 'nvim-treesitter.configs'.setup {
+        ensure_installed = { 'c', 'cpp', 'lua', 'rust', 'typescript', 'vimdoc', 'vim', 'java', 'scss', 'bash', 'html', 'markdown', 'yaml', 'w' },
         auto_install = true,
         indent = { enable = true, disable = { 'python' } },
         highlight = {
@@ -14,5 +17,18 @@ return {
       }
     end
   },
-}
 
+  {
+    'antoinephilibert/tree-sitter-w',
+    config = function()
+      require('nvim-treesitter.parsers').get_parser_configs().w = {
+        install_info = {
+          url = 'https://github.com/antoinephilibert/tree-sitter-w',
+          files = { 'src/parser.c' },
+          branch = 'master'
+        },
+        filetype = 'wengine',
+      }
+    end
+  }
+}
